318536386773c4867a9ee693ed22ae9f4c45374d
AndresiniGiovanni <gio.andresini@hotmail.it>
2022-11-09 15:09:38 +0100

    esercizio

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <AppTitleComponent />
+</template>
+<script>
+import AppTitleComponent from "./components/AppTitleComponent.vue";
+export default {
+  components: {
+    AppTitleComponent,
+  },
+};
+</script>
+<style lang="css"></style>
---
+++ src/components/AppTitleComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1 class="titolo">{{ titolo }}</h1>
+</template>
+
+<script>
+export default {
+  name: "AppTitleComponent",
+  data() {
+    return {
+      titolo: "La mia prima app con Vite!",
+    };
+  },
+};
+</script>
+
+<style lang="css" scoped>
+.titolo {
+  text-align: center;
+  margin-top: 8rem;
+  font-size: 40px;
+  color: white;
+}
+</style>
